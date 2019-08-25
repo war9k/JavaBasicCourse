25c51e14f8a9e7e21c7ac01208ba6de716fec308
Alex <translate86@gmail.com>
2019-08-25 16:19:12 +0300

    Lesson 5

---
+++ src/Lesson5/Square.java
@@ -0,0 +1,21 @@
+package Lesson5;
+
+public class Square {
+    public static final double PI = Math.PI;
+
+    public static int getPerimeter(int a) {
+        return a * 4;
+    }
+
+    public static int getSquare(int a) {
+        return a * a;
+    }
+
+    public static double getPerimeter(int a) {
+        return 2 * PI * a;
+    }
+
+    public static int getSquare(int a) {
+        return a * a;
+
+}
